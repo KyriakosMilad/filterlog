2efd8a72236510572d2adf2c45c56c00d1949027
Kyriakos Milad <40418046+KyriakosMilad@users.noreply.github.com>
2021-11-21 00:24:26 +0200

    Refactor code

--- main.go
+++ main.go
@@ -9,43 +9,38 @@ import (
 	"time"
 )
 
-func main() {
-	path := flag.String("path", "", "The path to the log file that should be filtered")
-	filter := flag.String("filter", "", "Log filters to search for")
-	separator := flag.String("separator", "", "Separate between multiple filters")
-	outputFile := flag.Bool("output_file", false, "Which to output the result to file or not")
+type filterlog struct {
+	path      string
+	separator string
+	filters   []string
+	results   []string
+}
 
-	flag.Parse()
-
-	if *path == "" && *filter == "" {
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-
-	if *path == "" {
-		panic("path is required")
-	}
-
-	if *filter == "" {
-		panic("filter is required")
-	}
-
-	var filters []string
-
-	if *separator != "" && strings.Contains(*filter, *separator) {
-		filters = strings.Split(*filter, *separator)
-
-		// remove empty strings
-		for k, v := range filters {
-			if v == "" {
-				filters = append(filters[:k], filters[k+1:]...)
-			}
-		}
+func (fl *filterlog) new(path string, filters string, separator string) {
+	fl.path = path
+	fl.separator = separator
+	if separator != "" && strings.Contains(filters, separator) {
+		fl.filters = strings.Split(filters, separator)
+		fl.removeEmptyFilters()
 	} else {
-		filters = []string{*filter}
+		fl.filters = []string{filters}
 	}
+}
 
-	f, err := os.Open(*path)
+func (fl *filterlog) removeEmptyFilters() {
+	for i, v := range fl.filters {
+		if v == "" {
+			fl.filters = append(fl.filters[:i], fl.filters[i+1:]...)
+		}
+	}
+}
+
+func (fl *filterlog) addResult(result string) {
+	fl.results = append(fl.results, result)
+}
+
+func (fl *filterlog) search() {
+	f, err := os.Open(fl.path)
 	if err != nil {
 		panic(err)
 	}
@@ -59,61 +54,71 @@ func main() {
 
 	r := bufio.NewReader(f)
 
-	// check if outputFile == true here better than checking it every line inside the loop
-	if *outputFile {
-		fileName := "filer_log_" + time.Now().Format("2006-01-02_15:04:05") + ".log"
-		outputFileName, err := os.Create(fileName)
+	for {
+		s, err := r.ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			} else {
+				panic(err)
+			}
+		}
 
+		for _, f := range fl.filters {
+			if strings.Contains(s, f) {
+				fl.addResult(s)
+				fmt.Print(s)
+				break
+			}
+		}
+	}
+}
+
+func (fl *filterlog) exportResults() {
+	fileName := "filerlog_" + time.Now().Format("2006-01-02_15:04:05") + ".log"
+	file, err := os.Create(fileName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(f *os.File) {
+		err := f.Close()
 		if err != nil {
 			panic(err)
 		}
+	}(file)
 
-		defer func(f *os.File) {
-			err := f.Close()
-			if err != nil {
-				panic(err)
-			}
-		}(outputFileName)
-
-		for {
-			s, err := r.ReadString('\n')
-			if err != nil {
-				if err.Error() == "EOF" {
-					break
-				} else {
-					panic(err)
-				}
-			}
-
-			for _, f := range filters {
-				if strings.Contains(s, f) {
-					fmt.Print(s)
-					_, err := outputFileName.WriteString(s)
-
-					if err != nil {
-						panic(err)
-					}
-					break
-				}
-			}
+	for _, v := range fl.results {
+		_, err := file.WriteString(v)
+		if err != nil {
+			panic(err)
 		}
-	} else {
-		for {
-			s, err := r.ReadString('\n')
-			if err != nil {
-				if err.Error() == "EOF" {
-					break
-				} else {
-					panic(err)
-				}
-			}
+	}
+}
 
-			for _, f := range filters {
-				if strings.Contains(s, f) {
-					fmt.Print(s)
-					break
-				}
-			}
-		}
+func main() {
+	path := flag.String("path", "", "The path to the log file that should be filtered")
+	filters := flag.String("filters", "", "Log filters to search for")
+	separator := flag.String("separator", "", "Separate between multiple filters")
+	exportResults := flag.Bool("export_results", false, "Which to print the results to a file or not")
+
+	flag.Parse()
+	if *path == "" && *filters == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if *path == "" {
+		panic("path is required")
+	}
+	if *filters == "" {
+		panic("one filter is required at least")
+	}
+
+	fl := &filterlog{}
+	fl.new(*path, *filters, *separator)
+	fl.search()
+	if *exportResults {
+		fl.exportResults()
 	}
 }
